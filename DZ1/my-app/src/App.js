f33b7a23cfe9165154a3aab98ffa0557956010a7
Erlan <104150226+111121E@users.noreply.github.com>
2022-11-14 17:35:26 +0600

    Add files via upload

---
+++ DZ1/my-app/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+
+function AboutUs () {
+    return (
+        <>
+            <h1>AboutUs</h1>
+        </>
+    );
+}
+
+function Portfolio () {
+    return (
+        <>
+            <h1>Portfolio</h1>
+        </>
+    );
+}
+
+
+
+function App() {
+  return (
+    <div className="App">
+        <AboutUs/>
+        <Portfolio/>
+
+    </div>
+  );
+}
+
+export default App;
