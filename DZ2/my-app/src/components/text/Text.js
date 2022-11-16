5f435842db969486716d60b4b455a1750d4db84d
Erlan <104150226+111121E@users.noreply.github.com>
2022-11-16 23:56:09 +0600

    Add files via upload

---
+++ DZ2/my-app/src/components/text/Text.js
@@ -0,0 +1,14 @@
+
+
+
+function Text (props) {
+    return (
+        <>
+            <h1>Title: {props.textInfo.title}</h1>
+            <h2>Subtitle: {props.textInfo.subtitle}</h2>
+        </>
+    )
+}
+
+
+export default Text;
---
+++ DZ2/my-app/src/pages/aboutUs/AboutUs.js
@@ -0,0 +1,15 @@
+import Text from "../../components/text/Text";
+
+function AboutUs() {
+    const text  = {
+        title: "hello",
+        subtitle: "lorem lorem lorem"
+    };
+    return (
+        <>
+            <Text textInfo={text}/>
+        </>
+
+    );
+}
+export default AboutUs;
